e683d01ebc211dd25534c044b135339ebac2d6ee
Jorge <jorge@MacBook-Air-de-Jorge.local>
2023-06-08 18:44:46 -0300

    d

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,24 @@ import {
   CreditScoreOutlined,
 } from "@mui/icons-material";
 import { IOrder } from "../interfaces";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Typography } from "@mui/material";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 const Home: NextPage = () => {
-  const { data, error } = useSWR<IOrder[]>("/api/admin/orders");
+  // const { data, error } = useSWR<IOrder[]>("/api/admin/orders");
   const [total_, setTotal_] = useState(0);
+  const { error, isLoading, user } = useUser()
 
 
   return (
     <>
       <AdminLayout title="" subTitle={``} icon={<AirplaneTicketOutlined />}>
         <><Typography variant="h1">{
-        // data && data.reduce((a, b) => a + b, 0) )        
+          error
+        }</Typography></>
+        <><Typography variant="h1">{
+          user
         }</Typography></>
       </AdminLayout>
     </>
