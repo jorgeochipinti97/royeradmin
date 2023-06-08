69ffa7e6d7341ee5fc5789b4beded46b4a286962
Jorge <jorge@Air-de-Jorge.fibertel.com.ar>
2023-06-08 16:32:23 -0300

    d

--- models/Product.ts
+++ models/Product.ts
@@ -4,42 +4,54 @@ import { IProduct } from '../interfaces';
 
 const productSchema = new Schema({
     description: { type: String, required: true, default: '' },
-    personalization: { type: String },
+    personalization: { type: String},
     images: [{ type: String }],
     inStock: { type: Number, required: true, default: 0 },
     price: { type: Number, required: true, default: 0 },
+    sizes: [{
+        type: String,
+        enum: {
+            values: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL', 'Unique', '7.5', '8', '8.5', '9', '9.5', '10', '10.5', '11', '11.5', '12', '12.5', '13', '14', '15'],
+            message: '{VALUE} no es un tamaño válido'
+        }
+    }],
     slug: { type: String, required: true, unique: true },
     tags: [{ type: String }],
-    sizes: [{ type: String }],
     title: { type: String, required: true, default: '' },
     type: {
         type: String,
         enum: {
-            values: ['shirts', 'jacket', 'pants', 'hoodies', 'hats', 'mate', 'yerba', 'alfajores', 'wine', 'short', 'socks', 'wallet', 'purse', 'accessories', 'bag', 'espadrilles', 'footwear'],
+            values: ['shirts', 't-shirt', 'football shirt', 'jacket', 'pants', 'hoodies', 'hats', 'mate', 'yerba', 'alfajores', 'wine', 'short', 'socks', 'wallet', 'purse', 'accessories', 'bag', 'espadrilles', 'footwear'],
             message: '{VALUE} no es un tipo válido'
         },
         default: 'shirts'
-    }
+    },
+    talles: [{ 
+        size: { type: String },
+        stock: { type: Number }
+    }]
     ,
     gender: {
         type: String,
         enum: {
-            values: ['football', 'regional',],
+            values: ['men', 'women', 'kid', 'unisex', 'regionales', 'fashion'],
             message: '{VALUE} no es un genero válido'
         },
-        default: 'football'
+        default: 'women'
     },
     popular: {
+
+        type: Boolean, 
+        default: false,
+
+    },
+    destacados: {
 
         type: Boolean,
         default: false,
 
-    },
-    productosRelacionados: [{
-        title: { type: String },
-        image: { type: String },
-        description: { type: String }
-    }]
+    }
+    // agregar productos relacionados 
 }, {
     timestamps: true
 });
